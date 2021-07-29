7067f69102d00bbebf6f836b5a0648a202003e79
João Fernando Brigido <joaofbrigido@hotmail.com>
2021-07-29 18:03:52 -0300

    comeco do slide nos checklists

--- js/module/availableChecklists.js
+++ js/module/availableChecklists.js
@@ -8,8 +8,8 @@ export default function availableChecklists() {
   const activeClass = "active";
 
   // Começar com Animação e primeiro conteúdo.
-  checklistListContent[0].classList.add(activeClass);
-  checkListTitleList[0].classList.add(activeClass);
+  // checklistListContent[0].classList.add(activeClass);
+  // checkListTitleList[0].classList.add(activeClass);
 
   function activeChecklistTitle(index) {
     checkListTitleList.forEach((item) => {
@@ -24,7 +24,32 @@ export default function availableChecklists() {
     });
     checklistListContent[index].classList.add(activeClass);
     activeChecklistTitle(index);
+    clearInterval(automaticContentInterval);
   }
+
+  // let count = 0;
+  // function automaticallyChangeContent() {
+  //   checklistListContent[count].classList.add(activeClass);
+  //   checkListTitleList[count].classList.add(activeClass);
+  //   ++count;
+  //   setTimeout(() => {
+  //     checklistListContent[count - 1].classList.remove(activeClass);
+  //     checkListTitleList[count - 1].classList.remove(activeClass);
+  //   }, 1000);
+
+  //   if (count > checkListTitleList.length - 1) {
+  //     // clearInterval(automaticContentInterval);
+  //     count = 0;
+  //   }
+
+  //   console.log(count);
+  //   // console.log(checkListTitleList.length);
+  // }
+
+  // const automaticContentInterval = setInterval(
+  //   automaticallyChangeContent,
+  //   1000
+  // );
 
   checkListTitleList.forEach((item, index) => {
     item.addEventListener("click", () => changeContent(index));
